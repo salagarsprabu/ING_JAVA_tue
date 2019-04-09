8736bb7d8db5427fca3026e367dca2b3a6ce4c95
Kalyan Kumar <kalyankumar0553@gmail.com>
2019-04-09 18:28:38 +0530

    Add files via upload

---
+++ src/main/java/com/src/ingtradeapp/service/UsersService.java
@@ -0,0 +1,25 @@
+package com.src.ingtradeapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.src.ingtradeapp.model.Users;
+import com.src.ingtradeapp.respository.UsersRepository;
+
+@Service
+public class UsersService {
+
+	@Autowired
+	UsersRepository  usersRepository;
+	
+	public Users userDetails(String accNum) {
+		return usersRepository.findByAccountNumber(accNum); 
+	}
+	
+	public List<Users> getUsers() {
+		return usersRepository.findAll();
+	}
+
+}
