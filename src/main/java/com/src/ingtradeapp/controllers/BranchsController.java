180cdeea159c817ce1be10c29d31a2bae806afc9
techfreks <48522802+techfreks@users.noreply.github.com>
2019-04-09 16:05:42 +0530

    For CROSS origin

--- src/main/java/com/src/ingtradeapp/controllers/BranchsController.java
+++ src/main/java/com/src/ingtradeapp/controllers/BranchsController.java
@@ -3,6 +3,7 @@ package com.src.ingtradeapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.src.ingtradeapp.service.BranchsService;
 
 @RestController
 @RequestMapping("/branchs")
+@CrossOrigin
 public class BranchsController {
 	
 	@Autowired
--- src/main/java/com/src/ingtradeapp/controllers/LoginController.java
+++ src/main/java/com/src/ingtradeapp/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.src.ingtradeapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import com.src.ingtradeapp.service.LoginService;
 
 @RestController
 @RequestMapping("/loginsecure")
+@CrossOrigin
 public class LoginController {
 	
 	@Autowired
