e53c101dbc59c983369a4cb617d6abb87cc4afef
Prakash <prakashooty16@gmail.com>
2019-04-09 12:38:00 +0530

    For Model Files

---
+++ src/main/java/com/src/ingtradeapp/model/UsersBalance.java
@@ -0,0 +1,63 @@
+package com.src.ingtradeapp.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class UsersBalance implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", length = 11)
+	private Long id;
+
+	@Column
+	private Long userId;
+
+	@Column
+	private String accountNumber;
+
+	@Column
+	private Double balance;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}	
+}
