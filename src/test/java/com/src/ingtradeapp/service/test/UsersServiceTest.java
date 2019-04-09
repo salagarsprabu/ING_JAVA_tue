ed1dd7a212d0464c52ba79d6b599ba7378a90af2
techfreks <48522802+techfreks@users.noreply.github.com>
2019-04-09 18:42:27 +0530

    df

---
+++ src/test/java/com/src/ingtradeapp/service/test/UsersServiceTest.java
@@ -0,0 +1,32 @@
+package com.src.ingtradeapp.service.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.src.ingtradeapp.model.Users;
+import com.src.ingtradeapp.respository.LoginRepository;
+import com.src.ingtradeapp.respository.UsersRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class UsersServiceTest {
+	
+	@MockBean
+	private UsersRepository usersRepository;
+	
+	/*@Test
+	public void userDetailsTest() {
+		String accountNumber = "174158989";
+		Users user=new Users(accountNumber);
+		when(usersRepository.findByAccountNumber(accountNumber)).thenReturn(user);
+		assertEquals(accountNumber, user.getAccountNumber());
+	}*/
+	
+
+}
