0a110a7e1e68a33f22227849d89432fcef52612c
techfreks <48522802+techfreks@users.noreply.github.com>
2019-04-09 17:23:08 +0530

    users details

---
+++ src/main/java/com/src/ingtradeapp/service/UsersService.java
@@ -0,0 +1,20 @@
+package com.src.ingtradeapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.src.ingtradeapp.model.Users;
+import com.src.ingtradeapp.respository.UsersRepository;
+
+@Service
+public class UsersService {
+
+	@Autowired
+	UsersRepository  usersRepository;
+	
+	public Users userDetails(String accNum) {
+		
+		return usersRepository.findAllByAccountNumber(accNum); 
+		
+	}
+}
