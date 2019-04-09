1f3ace12bbf1c9a63ef0216708adb89f00933234
techfreks <48522802+techfreks@users.noreply.github.com>
2019-04-09 16:01:44 +0530

    For branches

--- src/main/java/com/src/ingtradeapp/controllers/BranchsController.java
+++ src/main/java/com/src/ingtradeapp/controllers/BranchsController.java
@@ -3,6 +3,7 @@ package com.src.ingtradeapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class BranchsController {
 	
 	@Autowired
 	BranchsService branchService;
+	@GetMapping("")
 	public List<Branchs> getAllBranchs() {
 		return branchService.getBranchs();
 	}
