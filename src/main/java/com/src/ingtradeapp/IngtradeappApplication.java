de4894c1fed55ec256839a2f9a883c8fdab6c96c
unknown <kalyankumar0553@gmail.com>
2019-04-10 11:08:51 +0530

    Initial Setup # 1

---
+++ src/main/java/com/src/ingtradeapp/IngtradeappApplication.java
@@ -0,0 +1,13 @@
+package com.src.ingtradeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngtradeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngtradeappApplication.class, args);
+	}
+
+}
