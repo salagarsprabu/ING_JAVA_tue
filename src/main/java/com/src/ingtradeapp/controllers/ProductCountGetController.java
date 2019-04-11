d2081aca90f2df558be8dde230e6b1ccb2651a2c
kiranrathos <kiran.16711@gmail.com>
2019-04-11 18:27:09 +0530

    Update ProductCountGetController.java
    @crosssorigin

--- src/main/java/com/src/ingtradeapp/controllers/ProductCountGetController.java
+++ src/main/java/com/src/ingtradeapp/controllers/ProductCountGetController.java
@@ -1,4 +1,4 @@
-package com.src.ingtradeapp.controllers;
+import org.springframework.web.bind.annotation.RestController;package com.src.ingtradeapp.controllers;
 
 import java.util.List;
 
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.src.ingtradeapp.model.ProductCountDTO;
 import com.src.ingtradeapp.services.ProductCountService;
 
 @RestController
 @RequestMapping("/count")
+@CrossOrigin
 public class ProductCountGetController {
 	
 	@Autowired
