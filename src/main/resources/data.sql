c52b06edda41300b37ea2bb1297408930d5a3e79
Kalyan Kumar <kalyankumar0553@gmail.com>
2019-04-11 17:52:00 +0530

    For data

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-truncate ingdb.product_groups;
-truncate ingdb.products;
-truncate ingdb.product_count;
-truncate ingdb.product_groups;
-insert into ingdb.product_groups values(1,0,'EN','Mortgage');
-insert into ingdb.product_groups values(2,0,'EN','Payments');
-insert into ingdb.product_groups values(3,0,'EN','Savings');
